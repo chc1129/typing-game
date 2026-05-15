b8d235e1a1cda961a4573d7af17e1669a2b0c0ec
chc1129 <chc1129@gmail.com>
2026-05-15 22:00:09 +0900

    code fix

--- js/ui.js
+++ js/ui.js
@@ -213,7 +213,7 @@ export function showResult() {
 
     // スコアに応じてコースター画像を決定する
     let coasterSrc;
-    if (state.score >= 25000) {
+    if (state.score >= 21000) {
         coasterSrc = "images/coaster_03.png"; // エクセレント
     } else if (state.score >= 10000) {
         coasterSrc = "images/coaster_02.png"; // グッド
